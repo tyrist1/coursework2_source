0445300ce0e235b92beff78b4227a8f425acb06c
Andrei <tyrist84@mail.ru>
2022-01-06 10:30:31 +0300

    шаг второй в процессе-выполнено

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, request, render_template, abort, url_for
-import os
-from utils import load_data, comments_go_to_post
+from flask import Flask, render_template
+
+from utils import load_data
 
 data, bookmarks, comments = load_data()
-# POST_PATH = "posts.json"
-# UPLOAD_FOLDER = "uploads/images"
-# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html', posts='posts')
- # 'posts'  = почему у тебя работает без кавычек
-app.run(debug=True)
+
+app.run()
--- utils.py
+++ utils.py
@@ -13,15 +13,15 @@ def load_data():
         bookmarks = json.load(fp)
     return posts, comments, bookmarks
 
-def comments_go_to_post(posts,comments):
+def comments_go_to_post(posts, comments):
 
-    for i, post in enumerate (posts):
+    for i, post in enumerate(posts):
         pk = post.get('pk') #  что за get?
         post_comments=[]
         for comment in comments:
             if comment.get("post_id") == pk:
                 post_comments.append(comment)
-        posts[i]['comments'] = post_comments
+        posts[i]["comments_count"] = len(post_comments)
     return posts
 
 
