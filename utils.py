ff2b6d98ba8ad135d942481bfa5c72bb9312b714
Andrei <tyrist84@mail.ru>
2022-01-06 11:05:19 +0300

    шаг до консультации не работает корректно, хотя копипастил у ВАС

--- utils.py
+++ utils.py
@@ -8,12 +8,12 @@ def load_data():
     # \n как сделать новую строку
     with open('data/comments.json', 'r', encoding='UTF-8') as fp:
         comments = json.load(fp)
-    posts = comments_go_to_post(posts, comments)
+    posts = prepare_post (posts, comments)
     with open('data/bookmarks.json', 'r', encoding='UTF-8') as fp:
         bookmarks = json.load(fp)
     return posts, comments, bookmarks
 
-def comments_go_to_post(posts, comments):
+def prepare_post(posts, comments):
 
     for i, post in enumerate(posts):
         pk = post.get('pk') #  что за get?
@@ -22,7 +22,17 @@ def comments_go_to_post(posts, comments):
             if comment.get("post_id") == pk:
                 post_comments.append(comment)
         posts[i]["comments_count"] = len(post_comments)
+
+        posts[i]["content"] = tagify_content(posts[i]["content"])
     return posts
 
+def tagify_content(content):
 
+    words = content.split(" ")
+    for i, word in enumerate(words):
+        if word.startswith("#"):
+            tag=word.replace("#", "")
+            link = f"<a href=' /tag/{tag}'>{word}</a>"
+            word[i] = link
+    return " ".join(words)
 
